8ce7e601943d991ca398034d5ddd70ece7945292
Matt Burke <git@mattburkedev.com>
2016-12-02 08:24:40 -0600

    remove dist folder

--- dist/app-92b3d90c01.js
+++
@@ -1,2 +0,0 @@
-!function(){console.log("Hello World")}();
-//# sourceMappingURL=maps/app-92b3d90c01.js.map
--- dist/app-b897146558.js
+++
@@ -1,2 +0,0 @@
-!function(){console.log("Hello World from a.js")}(),function(){console.log("Hello World from b.js")}();
-//# sourceMappingURL=maps/app-b897146558.js.map
